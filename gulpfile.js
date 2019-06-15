ac73ff98765b1ed3df556fcfd39d8354081a799f
novalu <novalu@novalu.info>
2019-06-15 11:31:25 +0200

    Refactor gulp and npm scripts. See README for changes in commands.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
 const gulp = require("gulp");
 const run = require("gulp-run-command").default;
 
-gulp.task("install-deps", run("npm run install-deps"));
-
+gulp.task("install", run("npm run install"));
 gulp.task("build", run("npm run build"));
-gulp.task("start", run("npm run start"));
-gulp.task("debug", run("npm run debug"));
-gulp.task("watch-run", run("npm run watch"));
-
 gulp.task("deploy", run("npm run deploy"));
 
-gulp.task("watch-build", () => {
+gulp.task("run-test", run("npm run run-test"));
+gulp.task("run-test-debug", run("npm run run-test-debug"));
+gulp.task("run-test-continuous", run("npm run run-test-continuous"));
+
+// build once everytime the sources has been changed
+gulp.task("build-continuous", () => {
   gulp.watch([
     "src/**/*.ts",
     "functions/src/**/*.ts"
   ], ["build"]);
 });
 
-gulp.task("build-and-watch", ["build", "watch-build"]);
+// build once, after sources was changed and execute after sources was transpiled
+gulp.task("build-and-run-continuous", ["build", "build-continuous", "run-test-continuous"]);
 
-gulp.task("default", ["build-and-watch"]);
+gulp.task("default", ["build-and-run-continuous"]);
